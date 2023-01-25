509eb2f4c87693bf6dc5683e732c20ba10908a42
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-01-25 09:37:31 -0300

    add funcion buscador por ciudad

--- src/Components/Buscador/Buscador.jsx
+++ src/Components/Buscador/Buscador.jsx
@@ -1,15 +1,38 @@
+import { useNavigate } from "react-router-dom";
 
 
 function Buscador() {
+
+  function capitalizeFirstLetter(string) {
+    return string.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+      return letter.toUpperCase();
+    });
+  }
+ 
+  const navigate = useNavigate()
+  const handlerCity = e => {
+    e.preventDefault();
+    const keyword = e.currentTarget.keyword.value;
+    const correctKeyword = capitalizeFirstLetter(keyword)
+    
+    
+    navigate(`/resultados/${correctKeyword}`)
+   
+    
+    
+  }
+
+
+
   return (
-    <form>
-        <label>
-            <input type="text" placeholder="city"/>
-        </label>
-        <label>
-            <input type="text" placeholder="add guest" />
-        </label>
-        <span>buscar</span>
+    <form onSubmit={handlerCity}>
+      <label>
+        <input  type="text" name="keyword" placeholder="city" />
+      </label>
+      <label>
+        <input type="text" placeholder="add guest" />
+      </label>
+      <button type="submit">buscar</button>
     </form>
   )
 }
